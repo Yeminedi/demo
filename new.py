621b0828fbb238cdd8c5387957af91e704d46f9a
Yeminedi <54448815+Yeminedi@users.noreply.github.com>
2023-02-23 15:26:54 +0530

    Create new.py
    added code

---
+++ new.py
@@ -0,0 +1,9 @@
+# Databricks Note book Source
+print ("Wallero Tech")
+
+# COMMAND ---------------
+
+print ("Unit No - 1803, Manjeera Trinity Corporate, JNTU-Hitech City Road, Kukatpally, Hyderabad - 500 072, Telangana")
+
+# COMMAND ---------------
+print ("Wallero TechNOLOGIES")
